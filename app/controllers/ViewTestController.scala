dce3fa1b456498df0918140641b027eab64d713b
shigaki daichi <d-shigaki@pro-seeds.co.jp>
2019-04-03 16:57:47 +0900

    wip

--- app/controllers/ViewTestController.scala
+++ app/controllers/ViewTestController.scala
@@ -6,6 +6,7 @@ import play.api._
 import play.api.mvc._
 import java.util.Random
 import models.ViewTestData
+import models.JankenCase
 
 @Singleton
 class ViewTestController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
@@ -17,5 +18,14 @@ class ViewTestController @Inject()(cc: ControllerComponents) extends AbstractCon
     }
     def forLoop() = Action { implicit request: Request[AnyContent] =>
       Ok(views.html.viewtest_forloop("loop")(new ViewTestData().getTopics()))
+    }
+    def janken(): JankenCase = {
+      val gcp = Array("グー", "チョキ", "パー")
+      return JankenCase(
+        gcp(new Random().nextInt(3)), gcp(new Random().nextInt(3))
+      )
+    }
+    def doJanken() = Action { implicit request: Request[AnyContent] =>
+      Ok(views.html.viewtest_janken("じゃんけん")(janken()))
     }
 }
---
+++ app/models/JankenCase.scala
@@ -0,0 +1,4 @@
+
+package models
+
+case class JankenCase(me: String, you: String)
