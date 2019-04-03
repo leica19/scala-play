d80c1deeeb513ad0a83316c4e74bf84b2275f2f3
shigaki daichi <d-shigaki@pro-seeds.co.jp>
2019-04-03 16:14:49 +0900

    wip

--- app/controllers/ViewTestController.scala
+++ app/controllers/ViewTestController.scala
@@ -4,10 +4,18 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import java.util.Random
+import models.ViewTestData
 
 @Singleton
 class ViewTestController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
     def index() = Action { implicit request: Request[AnyContent] =>
-      Ok(views.html.viewtest_index())
+      Ok(views.html.viewtest_index("dadada")(423))
+    }
+    def twoArgs() = Action { implicit request: Request[AnyContent] =>
+      Ok(views.html.viewtest_twoargs("twoargs")(new Random().nextInt(10)))
+    }
+    def forLoop() = Action { implicit request: Request[AnyContent] =>
+      Ok(views.html.viewtest_forloop("loop")(new ViewTestData().getTopics()))
     }
 }
--- app/models/ViewTestData.scala
+++ app/models/ViewTestData.scala
@@ -2,5 +2,9 @@
 package models
 
 class ViewTestData {
-    
+    def getTopics() = Array(
+        "スポーツ",
+        "政治",
+        "経済"
+    )
 }
